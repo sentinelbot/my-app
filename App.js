0744b966a0126e7c40206dd9b5abc998d271d6d4
sentinelbot <ikmazameti@gmail.com>
2020-10-02 01:53:49 +0000

    input lesson

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("isaac");
-  const handleClick = () => {
-    setName("jojo");
-    setPerson({ name: "esther", age: "16" });
-  };
-  const [person, setPerson] = useState({ name: "gifty", age: 14 });
+  const [age, setAge] = useState(12);
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}> It's time to have Fun!' </Text>
-      </View>
       <View style={styles.body}>
-        <Text> My name is {name} </Text>
+        <Text>Enter name</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="e.g John Case"
+          onChangeText={(name) => {
+            setName(name);
+          }}
+        />
+        <Text>Enter Age</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="e.g 78"
+          keyboardType="numeric"
+          onChangeText={(age) => {
+            setAge(age);
+          }}
+        />
         <Text>
-          My name is {person.name} and his age is {person.age}
+          My name is {name} and age is {age}
         </Text>
-      </View>
-      <View style={styles.btnContainer}>
-        <Button title="Update name" onPress={handleClick} />
       </View>
     </View>
   );
@@ -35,8 +41,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  header: { backgroundColor: "red", padding: 20 },
-  body: { backgroundColor: "yellow", marginTop: 20 },
-  boldText: { fontWeight: "bold" },
-  btnContainer: { marginTop: 20 },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 20,
+  },
 });
